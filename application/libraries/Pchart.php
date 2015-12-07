c0467429a70a8d17338b907bb6f58ddaa14b1437
dmitrik <shestakdima@gmail.com>
2015-12-07 15:25:07 +0200

    pChart library

---
+++ application/libraries/Pchart.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('Not allowed');
+
+/**
+ * pChart library class
+ */
+class Pchart {
+
+    public function __construct() {
+        /* pChart library inclusions */
+        require_once BASEPATH . '../application/libraries/pChart/class/pData.class.php';
+        require_once BASEPATH . '../application/libraries/pChart/class/pDraw.class.php';
+        require_once BASEPATH . '../application/libraries/pChart/class/pImage.class.php';
+    }
+
+    function pData() {
+        return new pData();
+    }
+
+    function pImage($xSize, $ySize, $dataSet = NULL, $transparentBackground = FALSE) {
+        return new pImage($xSize, $ySize, $dataSet, $transparentBackground);
+    }
+
+}
