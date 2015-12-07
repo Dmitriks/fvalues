7f6cbe77bcb6bb6fcaf49fa9e771c8aa82fd488b
dmitrik <shestakdima@gmail.com>
2015-12-07 18:49:18 +0200

    Chart 'Last 24h' for all values

--- application/controllers/Chart.php
+++ application/controllers/Chart.php
@@ -20,56 +20,84 @@ class Chart extends CI_Controller {
      * Index Page for this controller
      */
     public function index() {
+        $this->last24h();
+    }
+
+    /**
+     * Charts for last 24 hous
+     */
+    public function last24h() {
+        $this->load->helper('url');
         $this->load->library('pchart');
         $symbols = $this->symbol_model->get_symbol_names();
         $end = time();
         $begin = strtotime('-1 day', $end);
-        $values = $this->value_model->get_hour_values(1, $begin, $end);
-        $xPoints = array();
-        $yPoints = array();
-        foreach ($values as $value) {
-            $xPoints[] = date('H:i', $value['time']);
-            $yPoints[] = $value['bid'];
+        $data['charts'] = array();
+        foreach ($symbols as $symbolId => $symbolName) {
+            $values = $this->value_model->get_hour_values($symbolId, $begin, $end);
+            $xPoints = array();
+            $yPoints = array();
+            foreach ($values as $value) {
+                $xPoints[] = date('H:i', $value['time']);
+                $yPoints[] = $value['bid'];
+            }
+            $fileName = 'img/chart/' . 'last24h_' . str_replace(' ', '_', $symbolName) . '.png';
+            $this->_drawChart($xPoints, $yPoints, 800, 300, $symbolName, $fileName);
+            $data['charts'][] = $fileName;
         }
+        $this->load->view('chart', $data);
+    }
+
+    /**
+     * Draw chart
+     * 
+     * @param array $xPoints    Contains array of X points
+     * @param array $yPoints    Contains array of Y points
+     * @param int $xSize        Contains width
+     * @param int $ySize        Contains height
+     * @param string $title     Contains title
+     * @param string $fileName  Contains name of file
+     */
+    private function _drawChart($xPoints, $yPoints, $xSize, $ySize, $title, $fileName) {
         /* Create and populate the pData object */
         $MyData = $this->pchart->pData();
         $MyData->addPoints($yPoints, "Line 1");
         $lSettings = array("R" => 0, "G" => 0, "B" => 255, "Alpha" => 80);
         $MyData->setPalette("Line 1", $lSettings);
-        $MyData->setAxisName(0, "USD");
+        //$MyData->setAxisName(0, "USD");
         $MyData->addPoints($xPoints, "Dates");
         //$MyData->setSerieDescription("Dates", "Dates");
         $MyData->setAbscissa("Dates");
 
         /* Create the pChart object */
-        $myPicture = $this->pchart->pImage(700, 230, $MyData);
+        $myPicture = $this->pchart->pImage($xSize, $ySize, $MyData);
 
         /* Draw the background */
         $bSettings = array("R" => 170, "G" => 183, "B" => 87, "Dash" => 1, "DashR" => 190, "DashG" => 203, "DashB" => 107);
-        $myPicture->drawFilledRectangle(0, 0, 700, 230, $bSettings);
+        $myPicture->drawFilledRectangle(0, 0, $xSize, $ySize, $bSettings);
 
         /* Overlay with a gradient */
         $gSettings = array("StartR" => 219, "StartG" => 231, "StartB" => 139, "EndR" => 1, "EndG" => 138, "EndB" => 68, "Alpha" => 50);
-        $myPicture->drawGradientArea(0, 0, 700, 230, DIRECTION_VERTICAL, $gSettings);
+        $myPicture->drawGradientArea(0, 0, $xSize, $ySize, DIRECTION_VERTICAL, $gSettings);
 
         /* Turn of Antialiasing */
         $myPicture->Antialias = FALSE;
 
         /* Add a border to the picture */
-        $myPicture->drawRectangle(0, 0, 699, 229, array("R" => 0, "G" => 0, "B" => 0));
+        $myPicture->drawRectangle(0, 0, $xSize - 1, $ySize - 1, array("R" => 0, "G" => 0, "B" => 0));
 
         /* Write the chart title */
         $myPicture->setFontProperties(array("FontName" => BASEPATH . "../application/libraries/pChart/fonts/Forgotte.ttf", "FontSize" => 11));
-        $myPicture->drawText(150, 35, $symbols[1], array("FontSize" => 20, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
+        $myPicture->drawText(150, 35, $title, array("FontSize" => 20, "Align" => TEXT_ALIGN_BOTTOMMIDDLE));
 
         /* Set the default font */
-        $myPicture->setFontProperties(array("FontName" => BASEPATH . "../application/libraries/pChart/fonts/pf_arma_five.ttf", "FontSize" => 6));
+        $myPicture->setFontProperties(array("FontName" => BASEPATH . "../application/libraries/pChart/fonts/verdana.ttf", "FontSize" => 7));
 
         /* Define the chart area */
-        $myPicture->setGraphArea(60, 40, 650, 190);
+        $myPicture->setGraphArea(50, 40, $xSize - 30, $ySize - 40);
 
         /* Draw the scale */
-        $scaleSettings = array("XMargin" => 0, "YMargin" => 0, "Floating" => TRUE, "DrawSubTicks" => TRUE, "CycleBackground" => TRUE, 'LabelRotation' => 90);
+        $scaleSettings = array("Floating" => TRUE, "DrawSubTicks" => TRUE, "CycleBackground" => TRUE, 'LabelRotation' => 90);
         $myPicture->drawScale($scaleSettings);
 
         /* Turn on Antialiasing */
@@ -79,7 +107,7 @@ class Chart extends CI_Controller {
         $myPicture->drawLineChart(array("DisplayValues" => TRUE));
 
         /* Render the picture (choose the best way) */
-        $myPicture->autoOutput("pictures/example.drawLineChart.simple.png");
+        $myPicture->render($fileName);
     }
 
 }
---
+++ application/views/chart.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Last 24 hours charts</title>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>css/styles.css"></link>
+    </head>
+    <body>
+
+        <div id="container">
+            <h1>Last 24 hours charts</h1>
+            <div id="body">
+
+                <?php foreach ($charts as $chart) : ?>
+                    <div class="chart">
+                        <img class="chart" src="<?php echo base_url() ?><?php echo $chart ?>"/>
+                    </div>
+                <?php endforeach; ?>
+
+            </div>
+            <div class="footer"><div class="source">Source: <a href="https://www.instaforex.com">https://www.instaforex.com</a></div>Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></div>
+        </div>
+    </body>
+</html>
