5814033be119c06971d0b7aab9aec590ec246393
dmitrik <shestakdima@gmail.com>
2015-12-22 15:08:37 +0200

    GBR, JPY, CHF (correct sort order)

--- application/sql/fvalues.sql
+++ application/sql/fvalues.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS `symbol` (
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-INSERT IGNORE INTO `symbol` VALUES (1,'EURUSD','Euro vs US Dollar'),(2,'GOLD','Gold'),(3,'SILVER','Silver'),(4,'#CL','Crude Oil Light Sweet'),(5,'#NG','Natural Gas'),(7,'GBPUSD','Great Britan vs US Dollar'),(8,'USDJPY','US Dollar vs Japanese Yen'),(9,'USDCHF','US Dollar vs Swiss Franc');
+INSERT IGNORE INTO `symbol` VALUES (1,'EURUSD','Euro vs US Dollar'),(2,'GBPUSD','Great Britan vs US Dollar'),(3,'USDJPY','US Dollar vs Japanese Yen'),(4,'USDCHF','US Dollar vs Swiss Franc'),(5,'GOLD','Gold'),(6,'SILVER','Silver'),(7,'#CL','Crude Oil Light Sweet'),(8,'#NG','Natural Gas');
 
 CREATE TABLE IF NOT EXISTS `minute_value` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
