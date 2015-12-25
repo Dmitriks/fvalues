f1995c97ae026dec514b16255fd8ab031b239a68
dmitrik <shestakdima@gmail.com>
2015-12-25 10:04:31 +0200

    Check off-time

--- application/views/chart.php
+++ application/views/chart.php
@@ -2,9 +2,13 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <div id="body">
-    <?php foreach ($charts as $chart) : ?>
-        <div class="chart">
-            <img class="chart" src="<?php echo base_url() ?><?php echo $chart ?>"/>
-        </div>
-    <?php endforeach; ?>
+    <?php if (empty($charts)) : ?>
+        Off-time
+    <?php else: ?>
+        <?php foreach ($charts as $chart) : ?>
+            <div class="chart">
+                <img class="chart" src="<?php echo base_url() ?><?php echo $chart ?>"/>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </div>
