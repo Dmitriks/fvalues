67cfc9dd10e39c3f4cdb661e9fe84d4e3bd11041
dmitrik <shestakdima@gmail.com>
2016-03-03 17:51:38 +0200

    Exclude USDUAH from analysis

--- application/views/analysis.php
+++ application/views/analysis.php
@@ -6,7 +6,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <tr>
             <td>EURUSD</td>
             <td>GBPUSD</td>
-            <td>USDUAH</td>
             <td>USDRUB</td>
             <td>Gold</td>
             <td>Silver</td>
@@ -15,7 +14,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </tr>
         <?php foreach (array('month', 'day', 'hour') as $period) : ?>
             <tr>
-                <?php foreach (array('EURUSD', 'GBPUSD', 'USDUAH', 'USDRUR', 'GOLD', 'SILVER', 'CL', 'USDX') as $symbol) : ?>
+                <?php foreach (array('EURUSD', 'GBPUSD', 'USDRUR', 'GOLD', 'SILVER', 'CL', 'USDX') as $symbol) : ?>
                     <td>
                         <a href="<?php echo base_url() ?>analysis/symbol/<?php echo $symbol ?>/<?php echo $period ?>"><?php echo ucfirst($period) ?></a>
                     </td>
